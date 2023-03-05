f756e7a37a7370f679fbd2f0032833be9a619314
Dmitry Matasov <dmatasov@bestplace.pro>
2023-03-05 22:21:12 +0400

    deploy

---
+++ deploy.zsh
@@ -0,0 +1,13 @@
+cd /opt/FamilyTree
+git pull
+
+cd /opt/digital-garden
+rm -rf posts
+mkdir posts
+
+cp -R ../FamilyTree/* posts
+rm -rf posts/☁️\ Облако posts/⚙️\ Шаблоны posts/✅\ План.md posts/📆 Календарь.md
+docker build -t obsidian-wiki .
+
+cd /opt/photoprism
+docker-compose up -d
